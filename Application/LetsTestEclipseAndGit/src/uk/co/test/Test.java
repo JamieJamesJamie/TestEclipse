2364795731843cd74329f5fcf7470eb86a0ed01e
Jamie Harris <James.Harris.2015@live.rhul.ac.uk>
2017-07-08 23:16:16 +0100

    Here's the eclipse project!

---
+++ Application/LetsTestEclipseAndGit/src/uk/co/test/Test.java
@@ -0,0 +1,19 @@
+package uk.co.test;
+
+/**
+ * @author Jamie
+ * 
+ * Test class for testing git and stuff
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 * 
+	 * It's a main method!
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi!");
+	}
+
+}
